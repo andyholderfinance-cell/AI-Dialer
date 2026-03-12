0043f3f65352c495fe49941f31fcc7893ae8d1ca
andyholderfinance-cell <andyholder.finance@gmail.com>
2026-03-12 17:13:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,19 +29,8 @@ const client = twilio(accountSid, authToken);
 app.post("/dial", async (req, res) => {
   try {
     const phone = req.body.phone;
-    
-app.get("/testdial", async (req, res) => {
 
-  const phone = "+1YOURNUMBER";
-
-  const call = await client.calls.create({
-    to: phone,
-    from: twilioNumber,
-    url: `https://${req.headers.host}/voice`
-  });
-
-  res.send("Dialing now...");
-});    const call = await client.calls.create({
+    const call = await client.calls.create({
       to: phone,
       from: twilioNumber,
       url: `https://${req.headers.host}/voice`
@@ -74,6 +63,18 @@ app.get("/", (req, res) => {
   res.send("AI dialer is running.");
 });
 
+app.get("/testdial", async (req, res) => {
+
+  const phone = "+18175842356";
+
+  const call = await client.calls.create({
+    to: phone,
+    from: twilioNumber,
+    url: `https://${req.headers.host}/voice`
+  });
+
+  res.send("Dialing now...");
+});
 // Twilio opens a WebSocket here for the live conversation
 wss.on("connection", (ws) => {
   console.log("Twilio connected to /conversationrelay");
