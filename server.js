38dcc797e3935698daab815febfaa141a61e186b
andyholderfinance-cell <andyholder.finance@gmail.com>
2026-03-12 13:57:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.post("/voice", (req, res) => {
   const twiml = new VoiceResponse();
 
-  twiml.say("Hello. This is your AI dialer test call.");
+  twiml.say("Hello. This is your A I dialer test call.");
 
   res.type("text/xml");
   res.send(twiml.toString());
