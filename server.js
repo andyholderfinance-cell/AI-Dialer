7038308297fc97524eca9de8cfb8d6d1f5d16148
andyholderfinance-cell <andyholder.finance@gmail.com>
2026-03-13 09:32:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get("/", (req, res) => {
 // Simple outbound test route
 app.get("/testdial", async (req, res) => {
   try {
-    const phone = "+18175842356";
+    const phone = "+13526675872";
 
     const call = await client.calls.create({
       to: phone,
