9aef34c80adc6b3cee616311ddcb6fba4fac1f63
andyholderfinance-cell <andyholder.finance@gmail.com>
2026-03-14 12:38:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1424,7 +1424,7 @@ app.post("/voice", (req, res) => {
     <ConversationRelay
       url="wss://${host}/conversationrelay?leadId=${leadId}"
       ttsProvider="ElevenLabs"
-      voice="UgBBYS2sOqTuMpoF3BR0-flash_v2_5-0.85_0.75_0.80"
+      voice="OYTbf65OHHFELVut7v2H-flash_v2_5-0.85_0.75_0.80"
       language="en-US"
       ttsLanguage="en-US"
     />
